df536feed3c24b92d2e9b76642b0c44f87420976
Damely Tineo <damely.tineo07@gmail.com>
2020-01-18 02:32:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+class EmailAddressParser
+  
+  attr_accessor :email_addresses
+  
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+  
+  def parse #parse new emails into array
+   email_addresses.split(/, | /).uniq
+  end
+   
+end
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match #parse new emails into array
+   
+  end
+   
+end
